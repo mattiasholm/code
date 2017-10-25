4815fd8c8d00ae93abb6e5136ce10059836b1a3e
Mattias Holm <mattias.holm@live.com>
2017-10-25 10:37:57 +0200

    Jobbat v idare med Trigger Webhook (OpsGenie)

--- Trigger Webhook (OpsGenie).ps1
+++ Trigger Webhook (OpsGenie).ps1
@@ -33,40 +33,60 @@
 
 
 $WebhookUrl = "https://api.opsgenie.com/v1/json/azure?apiKey=d9df92f1-a024-4bb9-a251-099b63838f3e"
-$JsonData = ""
+
+$body = @{
+  condition_window_size = "5"
+  condition_threshold = "1"
+}
+
+ConvertTo-Json $body
 
 
+
+
+$JsonData = @"
 {
-    "_incomingData": {
-      "_parsedData": {
-        "condition_window_size": "null",
-        "condition_threshold": "null",
-        "condition_time_aggregation": "null",
-        "condition_operator": "null",
-        "condition_type": "null",
-        "resource_type": "null",
-        "description": "null",
-        "resource_group_name": "null",
-        "condition_metric_name": "null",
-        "subscription_id": "null",
-        "portal_link": "null",
-        "condition_metric_unit": "null",
-        "delayIfDoesNotExists": "true",
-        "name": "null",
-        "condition_metric_value": "null",
-        "resource_id": "null",
-        "id": "null",
-        "resource_name": "null",
-        "resource_region": "null",
-        "timestamp": "null",
-        "status": "null"
-      },
-      "integrationType": "Azure",
-      "integrationName": "Azure - Donald Duck Ltd - Critical - SLA Platinum",
-      "incomingDataId": "148f2ef0-143b-4780-a127-197d33187ba9"
-    }
+  "_incomingData": {
+    "_parsedData": {
+      "condition_window_size": "5",
+      "condition_threshold": "1",
+      "condition_time_aggregation": "Average",
+      "alertSource": "Azure[Azure - Donald Duck Ltd - Critical - SLA Platinum-Create Alert] with incomingDataId[37b54d5c-55d1-4d78-8865-0dada609f24c] using policy/policies[OverwriteQuietHours]",
+      "condition_operator": "GreaterThan",
+      "condition_type": "Metric",
+      "resource_type": "microsoft.web/serverfarms",
+      "description": "",
+      "resource_group_name": "B3Studio",
+      "condition_metric_name": "CPU Percentage",
+      "subscription_id": "18acfa7a-785b-4c49-980d-b0ee07a7364a",
+      "portal_link": "https://portal.azure.com/#resource/subscriptions/18acfa7a-785b-4c49-980d-b0ee07a7364a/resourceGroups/B3Studio/providers/Microsoft.Web/serverfarms/B3StudioTestPlan",
+      "condition_metric_unit": "Percent",
+      "delayIfDoesNotExists": "true",
+      "name": "Test OpGenie",
+      "condition_metric_value": "4.2",
+      "resource_id": "/subscriptions/18acfa7a-785b-4c49-980d-b0ee07a7364a/resourceGroups/B3Studio/providers/Microsoft.Web/serverfarms/B3StudioTestPlan",
+      "id": "/subscriptions/18acfa7a-785b-4c49-980d-b0ee07a7364a/resourceGroups/B3Studio/providers/microsoft.insights/alertrules/Test%20OpGenie",
+      "resource_name": "b3studiotestplan",
+      "resource_region": "West Europe",
+      "timestamp": "2017-10-23T11:12:48.1764118Z",
+      "status": "Activated"
+    },
+    "integrationType": "Azure",
+    "integrationName": "Azure - Donald Duck Ltd - Critical - SLA Platinum",
+    "incomingDataId": "37b54d5c-55d1-4d78-8865-0dada609f24c"
+  },
+  "_result": {
+    "alertMessage": "[Azure] CPU Percentage GreaterThan 1 Metric in the last 5 mins was activated for sites: West Europe/b3studiotestplan",
+    "alertAction": "Create",
+    "integrationName": "Azure - Donald Duck Ltd - Critical - SLA Platinum",
+    "alertCount": "1",
+    "integrationActionName": "Create Alert",
+    "alertId": "92baa14f-62fb-4bed-b438-1a26cac69fbe",
+    "alertAlias": "/subscriptions/18acfa7a-785b-4c49-980d-b0ee07a7364a/resourceGroups/B3Studio/providers/microsoft.insights/alertrules/Test%20OpGenie - /subscriptions/18acfa7a-785b-4c49-980d-b0ee07a7364a/resourceGroups/B3Studio/providers/Microsoft.Web/serverfarms/B3StudioTestPlan",
+    "user": "Azure"
   }
-
+}
+"@
 
 # SKAPA EN AZURE-TRIGGERED ALERT OCH TITTA DÄREFTER PÅ JSON RAW FÖR ATT SKAPA CUSTOM ALERT!
 
