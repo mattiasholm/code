b606e9336de478b26ecc2d1579626f4cf9997e25
Mattias Holm <mattias.holm@live.com>
2017-11-02 14:59:21 +0100

    Added new script

---
+++ Office 365/Connect to Exchange Online.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy RemoteSigned -Force
+
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session
