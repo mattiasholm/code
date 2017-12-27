64424746ad41ac1016efb8ad34343769aa03fe59
Mattias Holm <mattias.holm@live.com>
2017-12-27 13:07:12 +0100

    SQL update

--- SQL/Lathund.sql
+++ SQL/Lathund.sql
@@ -1,6 +1,31 @@
 -- Lista samtliga ärenden
 SELECT IssueID,Subject,Body,IssueDate FROM hdIssues;
 
+SELECT IssueID,Subject,Body,CONVERT(DATETIME2(0), IssueDate) AS 'Date' FROM hdIssues;
+
+DECLARE @VAR DATETIME = '2012-03-27 12:34:39.807';
+
+SELECT @VAR, CONVERT(VARCHAR(5),@VAR, 108);
+
+-- Lista ärenden från en viss dag
+SELECT IssueID,Subject,Body,IssueDate FROM hdIssues WHERE CONVERT(VARCHAR(10),IssueDate,121) = '2017-11-21'
+
+
+-- Lista ärenden nyare än en viss dag
+SELECT IssueID,Subject,Body,IssueDate FROM hdIssues WHERE IssueDate > '2017-10-01'
+
+
+-- Lista öppna ärenden
+SELECT IssueID,Subject,Body,IssueDate FROM hdIssues WHERE ResolvedDate LIKE '2017-06-05'
+
+
+
+select * from hdIssues;
+select * from hdCategories;
+
+
+
+
 
 -- Lista 10 ärenden med högst prioritet:
 SELECT TOP 10 IssueID,Subject,Body,IssueDate,Priority
@@ -20,8 +45,8 @@ SELECT Subject,LEN(Subject) AS 'Antal tecken' FROM hdIssues;
 SELECT AVG(LEN(Body)) 'Medellängd ärendetext' FROM hdIssues;
 
 
--- Lista alla ärende med Inskickat från och Tilldelat
-SELECT hdIssues.IssueID AS 'Ärendenummer', hdIssues.Subject AS 'Rubrik' ,hdIssues.Body AS 'Ärendetext',hdIssues.IssueDate AS 'Tid',t1.Email AS 'Inskickat från',t2.Email AS 'Tilldelat',
+-- Lista alla ärende med Inskickat från och Tilldelat, samt korrekt Prioritet och Kategori
+SELECT hdIssues.IssueID AS 'Ärendenummer', hdIssues.Subject AS 'Rubrik' ,hdIssues.Body AS 'Ärendetext',hdIssues.IssueDate AS 'Tid',t1.Email AS 'Inskickat från',t2.Email AS 'Tilldelat',t3.Name AS 'Kategori',
 CASE
     WHEN hdIssues.Priority = 2 THEN 'Kritisk'
     WHEN hdIssues.Priority = 1 THEN 'Hög'
@@ -32,6 +57,7 @@ CASE
 FROM hdIssues
 LEFT JOIN hdUsers t1 ON hdIssues.UserID=t1.UserID
 LEFT JOIN hdUsers t2 ON hdIssues.AssignedToUserID=t2.UserID
+LEFT JOIN hdCategories t3 ON hdIssues.CategoryID=t3.CategoryID
 ORDER BY Priority DESC;
 
 
