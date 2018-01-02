9e9f69736cf12e46a473156551557925d4a064f9
Mattias Holm <mattias.holm@live.com>
2018-01-02 12:18:23 +0100

    Added Function Remove-Alias

---
+++ Function_Remove-Alias.ps1
@@ -0,0 +1,13 @@
+function Remove-Alias {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [String]$Name
+    )
+    $Path = "Alias:\$Name"
+    if (Test-Path ($Path)) {
+        Remove-Item -Path $Path -Confirm:$false
+    }
+    else {
+        Throw "Alias $Name does not exist."
+    }
+}
