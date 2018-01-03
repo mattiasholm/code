2d671cbaa1532e0a56f68b831247d2fbe0303957
Mattias Holm <mattias.holm@live.com>
2018-01-03 12:06:03 +0100

    Updated Personnummer script

--- Personnummer Analyzer.ps1
+++ Personnummer Analyzer.ps1
@@ -1,11 +1,15 @@
-# Egen class för personnummer??? Hur verifiera indata endast nummer och streck? Regex?
+$Personnummer = '196805221281'
+$Personnummer = '19680522-1281'
+$Personnummer = '6805221281'
+$Personnummer = '680522-1281'
+$Personnummer = '680522-12812'
+$Personnummer = Read-Host -Prompt 'Please enter Personnummer'
 
-$Personnummer = '910324-3391'
-$Personnummer = '9103243391'
-$Personnummer = '19910324-3391'
-$Personnummer = '199103243391'
+if ($Personnummer -notmatch '^[0-9]{6,8}-?[0-9]{4}$') {
+    Throw "Invalid format: $Personnummer. Please use the format YYMMDD-XXXX."
+}
 
-$Kontrollsiffra = $Personnummer.Substring($Personnummer.Length - 4).Substring(2,1)
+$Kontrollsiffra = $Personnummer.Substring($Personnummer.Length - 2,1)
 
 if ($Kontrollsiffra % 2 -eq 0) {
     $Sex = 'Kvinna'
