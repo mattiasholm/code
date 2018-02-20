88151bd5b8ef0540e4a2f09d226117a75a9e69a7
Mattias Holm <mattias.holm@live.com>
2018-02-20 09:45:29 +0100

    Added alternative timeout mechanisms

--- Timespan timeout control.ps1
+++ Timespan timeout control.ps1
@@ -1,10 +1,55 @@
+# # # ALT 1:
+
 $StartDate = Get-Date
 $Timeout = [System.TimeSpan]::FromSeconds(30)
 
 while ($StartDate + $Timeout -gt (Get-Date))
 {
-    Write-Host -Object "Timeout not reached"
+    Write-Host -Object 'Timeout not reached'
     Start-Sleep -Seconds 3
 }
 
-Write-Host -Object "Timeout reached"
+Write-Host -Object 'Timeout reached'
+
+
+
+# # # ALT 2:
+$Timeout = (Get-Date).AddSeconds(30)
+
+while ($Timeout -gt (Get-Date))
+{
+    Write-Host -Object 'Timeout not reached'
+    Start-Sleep -Seconds 3
+}
+
+Write-Host -Object 'Timeout reached'
+
+
+
+# # # ALT 3:
+$Timer = [System.Diagnostics.Stopwatch]::StartNew()
+
+while ($Timer.Elapsed -lt [System.TimeSpan]::FromSeconds(30))
+{
+    Write-Host -Object 'Timeout not reached'
+    Start-Sleep -Seconds 3
+}
+
+Write-Host -Object 'Timeout reached'
+$Timer.Stop()
+
+
+
+# # # ALT 4:
+$Timeout = 30
+$Timer = [System.Diagnostics.Stopwatch]::StartNew()
+
+while ($Timer.Elapsed.Seconds -lt $Timeout)
+{
+    Write-Host -Object 'Timeout not reached'
+    Start-Sleep -Seconds 3
+}
+
+Write-Host -Object 'Timeout reached'
+$Timer.Stop()
+throw 'Action did not complete before timeout period.'
