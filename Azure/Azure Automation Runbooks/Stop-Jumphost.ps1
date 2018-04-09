ad9606cbfd7883156f2310d26baeee8e7ba09cbe
Mattias Holm <mattias.holm@live.com>
2018-04-09 12:09:13 +0200

    Modified Runbook

--- Azure/Azure Automation Runbooks/Stop-Jumphost.ps1
+++ Azure/Azure Automation Runbooks/Stop-Jumphost.ps1
@@ -3,4 +3,4 @@ $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
 -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 
-Stop-AzureRmVM -Name 'BLA-DR-JH01' -ResourceGroupName 'RG-Management-DR' -Force
+Stop-AzureRmVM -Name 'BLA-DR-JH01' -ResourceGroupName 'RG-Management-DR' -Force -StayProvisioned
