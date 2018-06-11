2864ebfa7b70984653e454732abfe16ab1336a72
Mattias Holm <32541628+mattiasholm@users.noreply.github.com>
2018-06-11 09:46:12 +0200

    b3.se script

--- Office 365/Domänbyte b3.se/UPN/Mailboxes/Lägg till b3.se-UPN på utvald grupp.ps1
+++ Office 365/Domänbyte b3.se/UPN/Mailboxes/Lägg till b3.se-UPN på utvald grupp.ps1
@@ -2,9 +2,9 @@ Connect-EXOPSSession
 Connect-MsolService
 
 
-$DynGroup = Get-DynamicDistributionGroup 'Alla anställda B3IT Cloud Services AB'
-$Mailboxes = Get-Recipient -RecipientPreviewFilter ($DynGroup.RecipientFilter)
+$DynGroup = Get-DynamicDistributionGroup 'Alla anställda B3IT Dalarna AB'
 
+$Mailboxes = Get-Recipient -RecipientPreviewFilter ($DynGroup.RecipientFilter)
 
 foreach ($Mailbox in $Mailboxes)
 {
@@ -17,11 +17,11 @@ foreach ($Mailbox in $Mailboxes)
         if ($NewUserPrincipalName -ne $Mailbox.UserPrincipalName)
         {
             Set-MsolUserPrincipalName -UserPrincipalName $Mailbox.UserPrincipalName -NewUserPrincipalName $NewUserPrincipalName
-            Write-Host "$($Mailbox.DisplayName) - updated to b3.se!"
+            Write-Host "$($Mailbox.DisplayName) - updated to: $NewUserPrincipalName"
         }
         else
         {
-            Write-Host "Skipping $($Mailbox.DisplayName)"
+            Write-Host -ForegroundColor Yellow "Skipping $($Mailbox.DisplayName)"
         }
         
     }
