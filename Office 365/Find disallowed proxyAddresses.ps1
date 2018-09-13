b89c4d48e373c5cc69d0db14576ecba32be0b64d
Mattias Holm <32541628+mattiasholm@users.noreply.github.com>
2018-09-13 11:58:19 +0200

    Added script "Find disallowed proxyAddresses"

---
+++ Office 365/Find disallowed proxyAddresses.ps1
@@ -0,0 +1,39 @@
+Connect-EXOPSSession
+
+$ErrorActionPreference = "Stop"
+
+Class Mailbox
+{
+    [String]$DisplayName
+    [String]$EmailAddresses
+}
+
+
+$Mailboxes = Get-Mailbox
+$ListOfMailboxes = @()
+
+
+foreach ($Mailbox in $Mailboxes)
+{
+    $EmailAddresses = $Mailbox.EmailAddresses -match "smtp:"
+    $DisallowedEmailAddresses = $null
+
+    foreach ($EmailAddress in $EmailAddresses)
+    {        
+        $LocalPart = $EmailAddress -replace "@.*$" -replace "^.*:"
+        if ($LocalPart -notmatch "\.")
+        {
+            $DisallowedEmailAddresses += $EmailAddress -replace "^.*:"
+        }
+    }
+
+    if ($DisallowedEmailAddresses)
+    {
+        $MailboxRow = New-Object Mailbox
+        $MailboxRow.DisplayName = $Mailbox.DisplayName
+        $MailboxRow.EmailAddresses = $DisallowedEmailAddresses
+        $ListOfMailboxes += $MailboxRow
+    }
+}
+
+$ListOfMailboxes | Export-Csv -Path "C:\Temp\Disallowed proxyAddresses.csv" -Encoding Unicode -Delimiter "," -NoTypeInformation
