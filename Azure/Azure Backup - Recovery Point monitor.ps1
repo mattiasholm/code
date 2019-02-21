cb77ebe471004b1e23b211481b66ed891bea1404
Mattias Holm <32541628+mattiasholm@users.noreply.github.com>
2019-02-21 08:30:41 +0100

    Updated PS scripts

--- Azure/Azure Backup - Recovery Point monitor.ps1
+++ Azure/Azure Backup - Recovery Point monitor.ps1
@@ -1,5 +1,9 @@
+### Updated to use new Az module, run script in PowerShell Core (pwsh)
+
+
+
 $ErrorActionPreference = 'Stop'
-Add-AzureRmAccount
+Login-AzAccount
 # mattias.holm@b3itadmin.onmicrosoft.com
 
 
@@ -8,27 +12,35 @@ $SubscriptionName = 'B3Care - Production'
 $TenantDomain = 'b3care.se'
 $VaultName = 'B3CARE-NL-BACKUP01'
 $ResourceGroupName = 'B3CARE-NL-BAC'
+$ThresholdHours = 36
 
 
 
 $TenantId = (Invoke-WebRequest -Uri "https://login.windows.net/$TenantDomain/.well-known/openid-configuration" | ConvertFrom-Json).token_endpoint.Split('/')[3]
-$SubscriptionId = (Get-AzureRmSubscription -TenantId $TenantId | Where-Object {$_.Name -eq $SubscriptionName}).Id
-Select-AzureRmSubscription -SubscriptionId $SubscriptionId -TenantId $TenantId
+$SubscriptionId = (Get-AzSubscription -TenantId $TenantId | Where-Object {$_.Name -eq $SubscriptionName}).Id
+Select-AzSubscription -SubscriptionId $SubscriptionId -TenantId $TenantId
 
 
 
-$Vault = Get-AzureRmRecoveryServicesVault -Name $VaultName -ResourceGroupName $ResourceGroupName
+$Vault = Get-AzRecoveryServicesVault -Name $VaultName -ResourceGroupName $ResourceGroupName
 
-Set-AzureRmRecoveryServicesVaultContext -Vault $Vault
-
-$Containers = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM
+$Containers = Get-AzRecoveryServicesBackupContainer -VaultId $Vault.Id -ContainerType AzureVM
 
 
-foreach ($Container in $Containers)
-{
-    $LatestRecoveryPoint = Get-AzureRmRecoveryServicesBackupItem -Container $Container -WorkloadType AzureVM | Select-Object LatestRecoveryPoint
+foreach ($Container in $Containers) {
+    $LatestRecoveryPoint = (Get-AzRecoveryServicesBackupItem -VaultId $Vault.Id -Container $Container -WorkloadType AzureVM).LatestRecoveryPoint
+
+    if ($LatestRecoveryPoint -lt [System.DateTime]::UtcNow.AddHours(-$ThresholdHours)) {
+        Write-Host -ForegroundColor Red "Backup NOK"
+    }
+    else {
+        Write-Host -ForegroundColor Green "Backup OK"
+    }
 }
 
 
 
-$LatestRecoveryPoint
+
+
+# MARS:
+$Containers = Get-AzRecoveryServicesBackupContainer -VaultId $Vault.Id -ContainerType Windows -BackupManagementType MARS
---
+++ Port Scanner.ps1
@@ -0,0 +1,20 @@
+$Port = 22
+$NetworkPrefix = '10.0.30.'
+$FirstIp = "1"
+$LastIp = "254"
+
+$IpArray = $FirstIp..$LastIp
+
+foreach ($Ip in $IpArray) {
+    $Test = Test-NetConnection -ComputerName $NetworkPrefix$Ip -Port $Port
+    if ($Test.TcpTestSucceeded -eq $true) {
+        Write-Host -ForegroundColor Green -Object "$($Test.RemoteAddress) is reachable on port $Port"
+    }
+    else {
+        Write-Host -ForegroundColor Red -Object "$($Test.RemoteAddress) is not reachable on port $Port"
+    }
+}
+
+
+# # # OBS: Snabbare variant:
+New-Object System.Net.Sockets.TCPClient -ArgumentList $NetworkPrefix$Ip, $Port
