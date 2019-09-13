b4c6cc2cf6acb54b608b9081b1d9413edb0c379a
Mattias Holm <32541628+mattiasholm@users.noreply.github.com>
2019-09-13 14:27:16 +0200

    Updated Add-Printer with PrinterPort check

--- Office 365/Intune/B3 internt/Intune.Configuration.B3CS.Printer.Windows.ps1
+++ Office 365/Intune/B3 internt/Intune.Configuration.B3CS.Printer.Windows.ps1
@@ -4,11 +4,15 @@ $PrinterDriver = 'PCL6 V4 Driver for Universal Print'
 
 
 
-if (!(Get-Printer | Where-Object { $_.PortName -eq $IpAddress })) {
+if (!(Get-Printer | Where-Object { $_.PortName -eq (Get-PrinterPort | Where-Object { $_.PrinterHostAddress -eq $IpAddress }).Name })) {
+
+    if (!(Get-PrinterPort | Where-Object { $_.PrinterHostAddress -eq $IpAddress })) {  
+        Add-PrinterPort -Name $IpAddress -PrinterHostAddress $IpAddress
+    }
 
     if (!(Get-PrinterDriver | Where-Object { $_.Name -eq $PrinterDriver })) {  
         Add-PrinterDriver -Name $PrinterDriver
     }
 
-    Add-Printer -Name $Name -PortName $IpAddress -DriverName $PrinterDriver
+    Add-Printer -Name $Name -PortName (Get-PrinterPort | Where-Object { $_.PrinterHostAddress -eq $IpAddress }).Name -DriverName $PrinterDriver
 }
