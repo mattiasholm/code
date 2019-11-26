2d819bfe6120dda1af2a3c7642bc280954a1fe2e
Mattias Holm <mattias.holm@live.com>
2019-11-27 00:42:01 +0100

    Added Homebrew scripts for Mac

---
+++ Mac/Homebrew-InstallFormulae.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+brew cask install visual-studio-code
+brew install git
+brew install azure-cli
+brew install kubernetes-cli
+brew cask install powershell
+brew cask install azure-data-studio
+brew cask install microsoft-azure-storage-explorer
+brew cask install docker
+brew cask install google-chrome
+brew cask install microsoft-office
+brew cask install microsoft-teams
+brew cask install skype-for-business
+brew cask install teamviewer
+brew cask install watchguard-mobile-vpn-with-ssl
+brew cask install avg-antivirus
+brew cask install dropbox
+brew cask install gimp
+brew cask install paintbrush
+brew cask install vlc
+brew cask install spotify
+brew cask install plex-media-server
+brew cask install openemu
+brew cask install transmission
