89d0eeb5946e8e25f29fda6185a6aa2c32a365c7
Mattias Holm <mattias.holm@live.com>
2019-12-09 10:38:06 +0100

    Added Python directory and test script

---
+++ Python/sum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+sum = x + y
+
+print("The sum of the numbers {0} and {1} is {2}".format(x, y, sum))
