17d190caa45ba332d7048eeeead9f9affc7f976c
Mattias Holm <mattias.holm@b3.se>
2019-12-12 11:50:28 +0100

    Added Python scripts

---
+++ Python/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""A short description of the module -- called a docstring."""
+ 
+# Here comes your imports
+ 
+# Here comes your (few) global variables
+ 
+# Here comes your class definitions
+ 
+# Here comes your function definitions
+ 
+def main():
+	"""Launcher."""
+	# init the GUI or anything else
+	pass
+ 
+if __name__ == "__main__":
+	main()
