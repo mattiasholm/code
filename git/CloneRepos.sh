3d3f0779f4f05e3ee650df53676acd7c71775990
Mattias Holm <mattias.holm@b3.se>
2019-12-12 16:30:52 +0100

    Added CloneRepos script for GitHub

---
+++ git/CloneRepos.sh
@@ -0,0 +1,30 @@
+urls=(
+    "https://github.com/mattiasholm/mattiasholm.git"
+    "https://github.com/hi3g-access/tre-se-deployment.git"
+)
+
+path=~/repos
+
+
+
+mkdir -p $path && cd $path
+
+for url in ${urls[@]}
+do
+
+name=$(basename $url | sed "s/.git$//")
+
+if [[ ! -d "$path/$name" ]]
+then
+echo "Want to clone $url? (y/n)"
+read confirm
+
+if [ $confirm == "y" ]
+then
+    echo -e "Cloning $name\n"
+    git clone $url
+fi
+else
+echo -e "Git repo $name already exists, skipping\n"
+fi
+done
