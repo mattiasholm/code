bd2983962da01d0f9058c49c68a8de34543ec401
Mattias Holm <mattias.holm@live.com>
2020-01-28 11:50:17 +0100

    README.md

--- Mac/Homebrew/InstallPackages.sh
+++ Mac/Homebrew/InstallPackages.sh
@@ -17,6 +17,7 @@ brew cask install teamviewer
 brew cask install watchguard-mobile-vpn-with-ssl
 brew cask install avg-antivirus
 brew cask install dropbox
+brew cask install google-backup-and-sync
 brew cask install gimp
 brew cask install paintbrush
 brew cask install vlc
---
+++ Mac/README.md
@@ -0,0 +1,14 @@
+# Cheat Sheet
+
+<br>
+
+## Go to folder:
+Cmd+Shift+G
+
+## Go to Home (~):
+Cmd+Shift+H
+
+## Force quit applications:
+Cmd+Alt+Esc 
+
+## FYLL PÅ!
