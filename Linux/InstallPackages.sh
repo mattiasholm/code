51635682b4840013d53a8d0445d6f8b64f539426
Mattias Holm <mattias.holm@live.com>
2020-02-06 10:10:09 +0100

    InstallPackages.sh

--- Linux/InstallPackages.sh
+++ Linux/InstallPackages.sh
@@ -12,4 +12,6 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https && curl -s ht
 
 curl -fsSL https://get.pulumi.com | sh
 
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && sudo apt update && apt-cache policy docker-ce && sudo apt install -y docker-ce
+sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && sudo apt update && apt-cache policy docker-ce && sudo apt install -y docker-ce
+
+sudo apt install -y jq
