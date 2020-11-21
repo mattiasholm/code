4a931d9481f52776aee4ac164d1c37f6ae402a48
Mattias Holm <mattias.holm@live.com>
2020-11-21 15:55:51 +0100

    mas

--- macOS/mas/InstallApps.sh
+++ macOS/mas/InstallApps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-mas install 634148309 # Logic Pro
+mas install 634148309  # Logic Pro
 mas install 1295203466 # Microsoft Remote Desktop
-mas install 408981434 # iMovie
+mas install 408981434  # iMovie
+mas install 1174039276 # MultiCraft
