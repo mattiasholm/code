5b2cac3a424dc62062b58d25dda266344f318be9
Mattias Holm <mattias.holm@live.com>
2021-03-01 12:37:18 +0100

    InstallPackages.ps1

--- windows/chocolatey/InstallPackages.ps1
+++ windows/chocolatey/InstallPackages.ps1
@@ -9,14 +9,16 @@ $userName = "Mattias Holm"
 $userEmail = "mattias.holm@live.com"
 
 choco install -y vscode
+choco install -y microsoft-windows-terminal
 choco install -y git ; git config --global user.name $userName ; git config --global user.email $userEmail ; git config --global credential.helper wincred
-choco install -y python3
 choco install -y powershell-core ; pwsh "$(git rev-parse --show-toplevel)/pwsh/InstallModules.ps1"
 powershell "$(git rev-parse --show-toplevel)/pwsh/InstallModules.ps1"
 choco install -y azure-cli ; az extension add -y --source https://azclishowdeployment.blob.core.windows.net/releases/dist/show_deployment-0.0.7-py2.py3-none-any.whl
 choco install -y terraform
 choco install -y pulumi
+choco install -y python3
 choco install -y jq
+choco install -y 7zip
 choco install -y kubernetes-cli
 choco install -y azure-data-studio
 choco install -y microsoftazurestorageexplorer
@@ -31,11 +33,7 @@ choco install -y gimp
 choco install -y vlc
 choco install -y spotify
 choco install -y nestopia
-choco install -y wsl
-choco install -y 7zip
 choco install -y winscp
 choco install -y vim
-choco install -y greenshot
 choco install -y filezilla
 choco install -y jabra-direct
-choco install -y microsoft-windows-terminal
