03ee8f8646b540b728a0574947b0faeed25bcbb4
Mattias Holm <mattias.holm@live.com>
2021-04-08 19:13:04 +0200

    GitHub Action

--- azure/bicep/deploy.sh
+++ azure/bicep/deploy.sh
@@ -6,6 +6,9 @@ subscriptionId="9b184a26-7fff-49ed-9230-d11d484ad51b"
 location="WestEurope"
 templateFile="main.bicep"
 
-az login
-az account set --subscription "${subscriptionId}"
-az deployment sub create --location "${location}" --template-file "${templateFile}" --confirm-with-what-if
+# az login
+# az account set --subscription "${subscriptionId}"
+# az deployment sub create --location "${location}" --template-file "${templateFile}" --confirm-with-what-if
+echo "${subscriptionId}"
+echo "${location}"
+echo "${templateFile}"
