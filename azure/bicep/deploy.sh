e2bbcd1165532197ce8104d3dbf3b8b5dcf18b46
Mattias Holm <mattias.holm@live.com>
2021-04-08 19:53:57 +0200

    GitHub Action

--- azure/bicep/deploy.sh
+++ azure/bicep/deploy.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-set -e +x
-
 subscriptionId="9b184a26-7fff-49ed-9230-d11d484ad51b"
 location="WestEurope"
 templateFile="main.bicep"
 
-# az login
-az login --service-principal --username "${appId}" --password "${password}" --tenant "${tenant}" &&
-    az account set --subscription "${subscriptionId}"
+set -e +x
 
-az deployment sub what-if --location "${location}" --template-file "${templateFile}"
-az deployment sub create --location "${location}" --template-file "${templateFile}"
+if [[ "$*" == *--pipeline* ]]; then
+    az login --service-principal --username "${appId}" --password "${password}" --tenant "${tenant}" &&
+        az account set --subscription "${subscriptionId}"
+else
+    az login &&
+        az account set --subscription "${subscriptionId}"
+fi
+
+az deployment sub what-if --location "${location}" --template-file "${templateFile}" &&
+    az deployment sub create --location "${location}" --template-file "${templateFile}"
