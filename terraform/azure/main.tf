1d8104bbddbd3c8be06ae1344d87ab7e2203c11c
Mattias Holm <mattias.holm@live.com>
2021-06-08 15:57:23 +0200

    Commented out required_version

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_version = "~> 0.15.0"
+  # required_version = "~> 0.15.0"
+  # required_version = "~> 1.0.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.59.0"
+      version = "~> 2.62.0"
     }
   }
 
