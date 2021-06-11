5d66cde653fc5c8f0b7a0ac9e9eaa523ed2bb2ad
Mattias Holm <mattias.holm@live.com>
2021-06-11 16:16:24 +0200

    main.td

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_version = "~> 1.0.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.62.0"
-    }
-  }
-
   backend "remote" {
     organization = "mattiasholm"
 
     workspaces {
       name = "code"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.62.0"
     }
   }
 }
