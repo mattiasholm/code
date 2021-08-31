b5efdc6563a3dbf67b2b73d7aa9d6f447635399f
Mattias Holm <mattias.holm@live.com>
2021-08-31 08:31:30 +0200

    Removed arm-ttk

--- azure/arm/lint.sh
+++ azure/arm/lint.sh
@@ -13,3 +13,19 @@ command="Test-AzTemplate -MainTemplateFile ${templateFile} -TemplatePath ${templ
 pwsh -Command "Import-Module -FullyQualifiedName ${path}; ${command}; if (\$error.Count) { exit 1 }"
 
 rm -rf "arm-ttk/" # Hinner exita innan körs nu! Bättre med permanent submodule!
+
+
+
+# hint: You've added another git repository inside your current repository.
+# hint: Clones of the outer repository will not contain the contents of
+# hint: the embedded repository and will not know how to obtain it.
+# hint: If you meant to add a submodule, use:
+# hint:
+# hint: 	git submodule add <url> azure/arm/arm-ttk
+# hint:
+# hint: If you added this path by mistake, you can remove it from the
+# hint: index with:
+# hint:
+# hint: 	git rm --cached azure/arm/arm-ttk
+# hint:
+# hint: See "git help submodule" for more information.
