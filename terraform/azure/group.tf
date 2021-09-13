83b5ccff646804b1f105fe1843aaf9c7997c74fa
Mattias Holm <mattias.holm@live.com>
2021-09-13 16:38:47 +0200

    Group owner

--- terraform/azure/group.tf
+++ terraform/azure/group.tf
@@ -1,9 +1,23 @@
-# data "azuread_client_config" "current" {}
+locals {
+  userId = data.azuread_user.user.id
+}
+
+data "azuread_user" "user" {
+  user_principal_name = "mattias.holm@azronnieb3it.onmicrosoft.com"
+}
 
 resource "azuread_group" "group" {
   display_name = "AzureRBAC-KeyVault"
-  # owners           = [data.azuread_client_config.current.object_id]
+  owners = [
+    local.userId
+  ]
   security_enabled = true
 }
 
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/group
+
+# Member current user?! Blir ju dock dumt i pipeline?
+
+# KV AP
+
+# Parameterisera!
