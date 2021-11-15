eb875bddab7c462f468e8b8acafd1935a1c31080
Mattias Holm <mattias.holm@live.com>
2021-11-15 11:14:33 +0100

    Updated Terraform pipeline

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -28,6 +28,6 @@ provider "azurerm" {
   subscription_id = "9b184a26-7fff-49ed-9230-d11d484ad51b"
 }
 
-    provider "azuread" {
+provider "azuread" {
   tenant_id = "9e042b3b-36c4-4b99-8236-728c73166cd9"
 }
