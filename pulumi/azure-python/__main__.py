3ae88116329e252e34cc86ed5da5125c212ed047
Mattias Holm <mattias.holm@live.com>
2021-11-29 14:53:24 +0100

    Improved for loop + stUrl export

--- pulumi/azure-python/__main__.py
+++ pulumi/azure-python/__main__.py
@@ -9,7 +9,6 @@ prefixStripped = prefix.replace('-', '').lower()
 tags = config.get_object('tags')
 
 stCount = config.get_int('stCount') or 1
-
 stKind = config.get('stKind') or 'StorageV2'
 stSku = config.get('stSku') or 'Standard_LRS'
 stPublicAccess = config.get_bool('stPublicAccess') or False
@@ -21,24 +20,26 @@ rg = resources.ResourceGroup('rg',
                              tags=tags
                              )
 
+st = []
 for i in range(0, stCount):
-    st = storage.StorageAccount(f'st{i}',
-                                account_name=f'st{prefixStripped}{str(i + 1).zfill(3)}',
-                                resource_group_name=rg.name,
-                                tags=tags,
-                                kind=stKind,
-                                sku=storage.SkuArgs(
-                                    name=stSku,
-                                ),
-                                allow_blob_public_access=stPublicAccess,
-                                enable_https_traffic_only=stHttpsOnly,
-                                minimum_tls_version=stTlsVersion
-                                )
+    st.append(storage.StorageAccount(f'st{i}',
+                                     account_name=f'st{prefixStripped}{str(i + 1).zfill(3)}',
+                                     resource_group_name=rg.name,
+                                     tags=tags,
+                                     kind=stKind,
+                                     sku=storage.SkuArgs(
+                                         name=stSku,
+                                     ),
+                                     allow_blob_public_access=stPublicAccess,
+                                     enable_https_traffic_only=stHttpsOnly,
+                                     minimum_tls_version=stTlsVersion
+                                     )
+              )
 
     storage.BlobContainer(f'container{i}',
                           container_name=f'container{prefixStripped}001',
-                          account_name=st.name,
+                          account_name=st[i].name,
                           resource_group_name=rg.name,
                           )
 
-    pulumi.export(f'stUrl{i}', st.primary_endpoints)
+pulumi.export('stUrl', [x.primary_endpoints for x in st])
