9285a5ef1c1c31954b6227f69c448ccb30dba42a
Mattias Holm <mattias.holm@live.com>
2022-02-10 08:47:44 +0100

    Pulumi TypeScript appi

--- pulumi/azure-typescript/index.ts
+++ pulumi/azure-typescript/index.ts
@@ -6,24 +6,18 @@ const config = new pulumi.Config
 const prefix = config.require('prefix')
 const tags: undefined = config.getObject('tags')
 
-// const planKind = config.get('planKind') || 'linux'
-// const planSku = config.get('planSku') || 'B1'
-// const planCapacity = config.getNumber('planCapacity') || 1
-// const planReserved = planKind == 'linux' ? true : false
+const appiKind = config.get('appiKind') || 'web'
+const appiType = config.get('appiType') || 'web'
 
 const rg = new azure.resources.ResourceGroup('rg', {
     resourceGroupName: `rg-${prefix}-001`,
     tags: tags
 })
 
-// const plan = new azure.web.AppServicePlan('plan', {
-//     name: `plan-${prefix}-001`,
-//     resourceGroupName: rg.name,
-//     tags: tags,
-//     kind: planKind,
-//     sku: {
-//         name: planSku,
-//         capacity: planCapacity
-//     },
-//     reserved: planReserved
-// })
+const appi = new azure.insights.Component('appi', {
+    resourceName: `appi-${prefix}-001`,
+    resourceGroupName: rg.name,
+    tags: tags,
+    kind: appiKind,
+    applicationType: appiType
+});
