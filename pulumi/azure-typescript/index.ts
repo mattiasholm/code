5c6b0d501d0612ec93802f41b0813fa1601f2d16
Mattias Holm <mattias.holm@live.com>
2022-02-11 17:13:21 +0100

    Pulumi TypeScript kv

--- pulumi/azure-typescript/index.ts
+++ pulumi/azure-typescript/index.ts
@@ -1,23 +1,44 @@
 import * as pulumi from '@pulumi/pulumi'
-import * as azure from '@pulumi/azure-native'
+import { authorization, resources, insights, keyvault } from '@pulumi/azure-native'
+import * as azuread from '@pulumi/azuread'
 
-const config = new pulumi.Config
+const config = new pulumi.Config()
 
 const prefix = config.require('prefix')
-const tags: undefined = config.getObject('tags')
+const prefixStripped = prefix.replace('-', '').toLowerCase()
+const tags: undefined = config.getObject('tags') // {[key: string]: string} https://www.pulumi.com/registry/packages/azure-native/api-docs/resources/tagatscope/
+const tenantId = pulumi.output(authorization.getClientConfig()).tenantId
 
 const appiKind = config.get('appiKind') || 'web'
 const appiType = config.get('appiType') || 'web'
 
-const rg = new azure.resources.ResourceGroup('rg', {
+const kvSku: keyvault.SkuName = config.get('kvSku') || 'standard'
+
+const rg = new resources.ResourceGroup('rg', {
     resourceGroupName: `rg-${prefix}-001`,
     tags: tags
 })
 
-const appi = new azure.insights.Component('appi', {
+const appi = new insights.Component('appi', {
     resourceName: `appi-${prefix}-001`,
     resourceGroupName: rg.name,
     tags: tags,
     kind: appiKind,
     applicationType: appiType
-});
+})
+
+const kv = new keyvault.Vault('kv', {
+    vaultName: `kv-${prefix}-001`,
+    resourceGroupName: rg.name,
+    tags: tags,
+    properties: {
+        tenantId: tenantId,
+        sku: {
+            family: 'A',
+            name: kvSku
+        },
+        accessPolicies: []
+    }
+})
+
+export const tmp = prefixStripped
