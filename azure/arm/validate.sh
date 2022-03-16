09adb59a1cc837d51fdf69783ee234cbfb85c7e0
Mattias Holm <mattias.holm@live.com>
2022-03-16 08:27:19 +0100

    Simplified deploy scripts

--- azure/arm/validate.sh
+++ azure/arm/validate.sh
@@ -1,42 +1,29 @@
 #!/usr/bin/env bash
 
-function SetVariables() {
-    set -e +x
-    subscriptionId='9b184a26-7fff-49ed-9230-d11d484ad51b'
-    resourceGroup='rg-holm-arm-001'
-    location='WestEurope'
-    tags=(
-        'Application=ARM'
-        'Company=Holm'
-        'Environment=Dev'
-        'Owner=mattias.holm@live.com'
-    )
-    operations=('validate' 'what-if')
-    templateFile='main.json'
-    parameterFile='main.parameters.json'
-}
+set -e +x
+subscriptionId='9b184a26-7fff-49ed-9230-d11d484ad51b'
+resourceGroup='rg-holm-arm-001'
+location='WestEurope'
+tags=(
+    'Application=ARM'
+    'Company=Holm'
+    'Environment=Dev'
+    'Owner=mattias.holm@live.com'
+)
+operations=('validate' 'what-if')
+templateFile='main.json'
+parameterFile='main.parameters.json'
 
-function CreateResourceGroup() {
-    az group create \
-        --name $resourceGroup \
-        --location $location \
-        --tags ${tags[*]}
-}
+az group create \
+    --name $resourceGroup \
+    --location $location \
+    --tags ${tags[*]} \
+    --subscription $subscriptionId
 
-function Deploy() {
-    for operation in ${operations[@]}; do
-        az deployment group $operation \
-            --subscription $subscriptionId \
-            --resource-group $resourceGroup \
-            --template-file $templateFile \
-            --parameters @$parameterFile
-    done
-}
-
-function main() {
-    SetVariables
-    CreateResourceGroup
-    Deploy
-}
-
-main
+for operation in ${operations[@]}; do
+    az deployment group $operation \
+        --subscription $subscriptionId \
+        --resource-group $resourceGroup \
+        --template-file $templateFile \
+        --parameters @$parameterFile
+done
--- azure/bicep/deploy.sh
+++ azure/bicep/deploy.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
 
-function SetVariables() {
-    set -e +x
-    subscriptionId='9b184a26-7fff-49ed-9230-d11d484ad51b'
-    location='WestEurope'
-    operations=('create')
-    templateFile='main.bicep'
-    parameterFile='main.parameters.json'
-}
+set -e +x
+subscriptionId='9b184a26-7fff-49ed-9230-d11d484ad51b'
+location='WestEurope'
+operations=('create')
+templateFile='main.bicep'
+parameterFile='main.parameters.json'
 
-function Deploy() {
-    for operation in ${operations[@]}; do
-        az deployment sub $operation \
-            --subscription $subscriptionId \
-            --location $location \
-            --template-file $templateFile \
-            --parameters @$parameterFile
-    done
-}
-
-function main() {
-    SetVariables
-    Deploy
-}
-
-main
+for operation in ${operations[@]}; do
+    az deployment sub $operation \
+        --subscription $subscriptionId \
+        --location $location \
+        --template-file $templateFile \
+        --parameters @$parameterFile
+done
