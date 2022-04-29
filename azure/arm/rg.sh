cdb3b9fd18426a7bf399b347b17ac3e20e55534a
Mattias Holm <mattias.holm@live.com>
2022-04-29 12:57:13 +0200

    Updated ARM pipeline

---
+++ azure/arm/rg.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e +x
+. main.config
+
+az group create --subscription $subscription --name $resourceGroup --location $location --tags ${tags[*]}
--- azure/arm/whatif.sh
+++ azure/arm/whatif.sh
@@ -3,5 +3,4 @@
 set -e +x
 . main.config
 
-az group create --subscription $subscription --name $resourceGroup --location $location --tags ${tags[*]}
 az deployment group what-if --subscription $subscription --resource-group $resourceGroup --template-file $template --parameters @$parameters
