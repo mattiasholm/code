112634df7b7b97a1c02e4ca5ec234e3c66cbde1c
Mattias Holm <mattias.holm@live.com>
2022-09-01 14:54:32 +0200

    Updated outputs

--- terraform/sp/outputs.tf
+++ terraform/sp/outputs.tf
@@ -1,3 +1,7 @@
+output "tenant_id" {
+  value = data.azuread_client_config.current.tenant_id
+}
+
 output "client_id" {
   value = azuread_application.app.application_id
 }
@@ -5,8 +9,4 @@ output "client_id" {
 output "client_secret" {
   value     = azuread_application_password.secret.value
   sensitive = true
-}
-
-output "tenant_id" {
-  value = data.azuread_client_config.current.tenant_id
 }
