1709c2d712af6b6a8f0e36ae63791be3c01b5c02
Mattias Holm <mattias.holm@live.com>
2022-09-07 11:45:42 +0200

    InstallApps.sh

--- macOS/mas/InstallApps.sh
+++ macOS/mas/InstallApps.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mas install 634148309  # Logic Pro
+mas install 408981434  # iMovie
 mas install 937984704  # Amphetamine
 mas install 1295203466 # Microsoft Remote Desktop
-mas install 1553936137 # Azure VPN Client
+mas install 634148309  # Logic Pro
