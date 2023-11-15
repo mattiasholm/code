f0c4c3a5001321bfb3e17a1904ad8a5abb16aea2
Mattias Holm <mattias.holm@live.com>
2023-11-15 15:38:37 +0100

    Added Azure VPN Client

--- macOS/mas/InstallApps.sh
+++ macOS/mas/InstallApps.sh
@@ -4,5 +4,6 @@ set -e
 
 mas install 408981434  # iMovie
 mas install 937984704  # Amphetamine
+mas install 1553936137 # Azure VPN Client
 mas install 1295203466 # Microsoft Remote Desktop
 mas install 634148309  # Logic Pro
