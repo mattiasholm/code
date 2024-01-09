bd017290ced1c538bdbc51479cef4f8b26a63a16
Mattias Holm <32541628+mattiasholm@users.noreply.github.com>
2024-01-09 17:27:23 +0100

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -2,6 +2,7 @@ locals {
   prefix          = lower("${var.tags.Company}-${var.tags.Application}")
   prefix_stripped = replace(local.prefix, "-", "")
   vnet_size       = split("/", var.vnet_address_prefix)[1]
+apa = 123
 }
 
 data "azurerm_subscription" "sub" {}
