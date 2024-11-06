e9f3653b697134ec7f0af1549e34fc0f9e89a495
Mattias Holm <mattias.holm@live.com>
2024-11-06 15:21:22 +0100

    Removed Pulumi TypeScript

--- pulumi/azure-typescript/config.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from '@pulumi/pulumi';
-// import * as azuread from '@pulumi/azuread'
-
-const config = new pulumi.Config();
-
-export const tags: { [key: string]: string } = config.requireObject('tags');
-export const prefix = `${tags.Company}-${tags.Application}`.toLowerCase()
--- pulumi/azure-typescript/index.ts
+++
@@ -1,9 +0,0 @@
-import { resources } from '@pulumi/azure-native'
-import * as config from './config';
-
-const rg = new resources.ResourceGroup('rg', {
-    resourceGroupName: `rg-${config.prefix}-01`,
-    tags: config.tags,
-});
-
-export const rgName = rg.name
