ea0090e9f5456186a4b4e2f2841c1869428f2981
Mattias Holm <mattias.holm@live.com>
2026-05-05 08:49:30 +0200

    Updated Bicep validate script

--- bicep/azure/validate.sh
+++ bicep/azure/validate.sh
@@ -3,4 +3,6 @@
 set -e
 . config.sh
 
-az stack sub validate --subscription $subscription --location $location --template-file $template --parameters $parameters --no-prompt --output table
+# az stack sub validate --subscription $subscription --location $location --template-file $template --parameters $parameters --no-prompt --output table
+# az stack sub validate --name $stack --subscription $subscription --location $location --template-file $template --parameters $parameters --deny-settings-mode None --action-on-unmanage deleteAll --yes --output table
+az stack sub validate --name $stack --deny-settings-mode None --action-on-unmanage deleteAll --yes --output table
